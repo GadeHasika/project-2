1d9c8567928bb5a6c86896cea2e993e510f9ca29
GadeHasika <160099681+GadeHasika@users.noreply.github.com>
2025-08-04 15:17:07 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,13 @@
+class Factorial{
+    public static long compute(int n){
+        long res=1;
+        for(int i=2;i<9;i++){
+            res*=i;
+        }
+        return res;
+    }
+    public static void main(String[]args){
+        int n=8;
+        System.out.println("factors of "+n+" is "+compute(n))
+    }
+}

e2445679ea61cf5847c53101596928340440102d
GadeHasika <160099681+GadeHasika@users.noreply.github.com>
2025-08-04 15:28:26 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,13 +1,13 @@
 class Factorial{
     public static long compute(int n){
         long res=1;
-        for(int i=2;i<9;i++){
+        for(int i=2;i<6;i++){
             res*=i;
         }
         return res;
     }
     public static void main(String[]args){
-        int n=8;
+        int n=5;
         System.out.println("factors of "+n+" is "+compute(n));
     }
 }
